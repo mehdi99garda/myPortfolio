99af602042aca5513459a63dcf74021a918d2d31
El Mehdi El Garda <39412604+mehdi99garda@users.noreply.github.com>
2023-03-08 16:13:29 +0100

    add two languages es,de and update the function

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,6 +1,8 @@
 var arrLang = new Array();
         arrLang['en'] = new Array();
         arrLang['fr'] = new Array();
+        arrLang['es'] = new Array();
+        arrLang['de'] = new Array();
 
         // English content
         arrLang['en']['hello'] = 'Hello, I am';
@@ -63,13 +65,95 @@ var arrLang = new Array();
         arrLang['fr']['send'] = 'Envoyer';
         arrLang['fr']['all'] = 'Tous';
 
-        // Process translation
-        $(function () {
-            $('.translate').click(function () {
-                var lang = $(this).attr('id');
+        //espanol content
+        arrLang['es']['hello'] = 'Hola, yo soy';
+        arrLang['es']['title'] = 'Desarrollador web y front-end móvil (Flutter) ';
+        arrLang['es']['print'] = 'Imprimir currículum';
+        arrLang['es']['home'] = 'bienvenida';
+        arrLang['es']['about'] = 'A proposito';
+        arrLang['es']['portofolio'] = 'portafolio';
+        arrLang['es']['resume'] = 'CV';
+        arrLang['es']['contact'] = 'Contacto';
+        arrLang['es']['year'] = 'años';
+        arrLang['es']['phone'] = 'Teléfono';
+        arrLang['es']['address'] = 'DIRECCIÓN';
+        arrLang['es']['who'] = 'Quién soy ?';
+        arrLang['es']['download'] = 'Descargar mi currículum';
+        arrLang['es']['personal_info'] = 'Información personal';
+        arrLang['es']['my'] = 'Mi';
+        arrLang['es']['resume'] = 'Viaje';
+        arrLang['es']['available'] = 'estoy disponible para freelance';
+        arrLang['es']['contact_me'] = 'Contácteme';
+        arrLang['es']['agile'] = 'método ágil';
+        arrLang['es']['expertise'] = 'Mi experiencia';
+        arrLang['es']['read_more'] = 'Leer más >';
+        arrLang['es']['latest'] = 'El último';
+        arrLang['es']['news'] = 'Noticias';
+        arrLang['es']['skills'] = 'HABILIDADES';
+        arrLang['es']['get_in_touch'] = 'Ponerse en contacto';
+        arrLang['es']['send_msg'] = 'Enviar un mensaje';
+        arrLang['es']['send'] = 'enviar a';
+        arrLang['es']['all'] = 'Todo';
 
-                $('.lang').each(function (index, item) {
-                    $(this).text(arrLang[lang][$(this).attr('key')]);
-                });
+        //germany content
+        arrLang['de']['hello'] = 'Hallo, ich bin';
+        arrLang['de']['title'] = 'Front-End Mobile (Flutter) und Web-Entwickler ';
+        arrLang['de']['print'] = 'Lebenslauf drucken';
+        arrLang['de']['home'] = 'Willkommen';
+        arrLang['de']['about'] = 'in Bezug auf';
+        arrLang['de']['portofolio'] = 'Geldbörse';
+        arrLang['de']['resume'] = 'CV';
+        arrLang['de']['contact'] = 'kontaktiere mich';
+        arrLang['de']['year'] = 'Jahre';
+        arrLang['de']['phone'] = 'Telefon';
+        arrLang['de']['address'] = 'Adresse';
+        arrLang['de']['who'] = 'Wer bin ich ?';
+        arrLang['de']['download'] = 'Laden Sie meinen Lebenslauf herunter';
+        arrLang['de']['personal_info'] = 'Persönliche Informationen';
+        arrLang['de']['my'] = 'Meine';
+        arrLang['de']['resume'] = 'berufliche Laufbahn';
+        arrLang['de']['available'] = 'Ich stehe freiberuflich zur Verfügung';
+        arrLang['de']['contact_me'] = 'kontaktiere mich';
+        arrLang['de']['agile'] = 'Agile Methode';
+        arrLang['de']['expertise'] = 'Meine Berufserfahrung';
+        arrLang['de']['read_more'] = 'Lesen Sie mehr >';
+        arrLang['de']['latest'] = 'neueste';
+        arrLang['de']['news'] = 'Nachricht';
+        arrLang['de']['skills'] = 'FÄHIGKEITEN';
+        arrLang['de']['get_in_touch'] = 'Einen Vetrag eingehen';
+        arrLang['de']['send_msg'] = 'Eine Nachricht schicken';
+        arrLang['de']['send'] = 'Schicken';
+        arrLang['de']['all'] = 'Alle';
+
+        // Process translation
+        // $(function () {
+        //     $('.translate').click(function () {
+        //         var lang = $(this).attr('id');
+
+        //         $('.lang').each(function (index, item) {
+        //             $(this).text(arrLang[lang][$(this).attr('key')]);
+        //         });
+        //     });
+        // });
+        $(function () {
+            // Set the initial language
+            var lang = 'en';
+          
+            // Update the language when a different option is selected
+            $('.langue').change(function () {
+              lang = $(this).val();
+              updateLanguage(lang);
             });
-        });
+          
+            // Update the text on the page to the given language
+            function updateLanguage(lang) {
+              $('.lang').each(function (index, item) {
+                $(this).text(arrLang[lang][$(this).attr('key')]);
+              });
+            }
+            
+            // Update the language on page load
+            updateLanguage(lang);
+          });
+          
+
